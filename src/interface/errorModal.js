be976d9c4f6048b2d3b1d3de19c167a067674fab
Luiz Henrique <luizboaventura24@gmail.com>
2024-12-23 14:35:51 -0300

    Add error modal

---
+++ src/interface/errorModal.js
@@ -0,0 +1,18 @@
+import UserLocation from "../services/getUserLocation.js";
+
+let errorModal = document.getElementById("errorModal");
+
+const timeout = setInterval(async () => {
+  const userLocation = new UserLocation();
+
+  const lon = await userLocation.getLon();
+  const lat = await userLocation.getLat();
+
+  if (typeof lon === "number" && typeof lat === "number") {
+    errorModal.style.display = "none";
+    clearInterval(timeout)
+  } else {
+    errorModal.style.display = "flex";
+    console.log("Desabilitado");
+  }
+}, 1000);
--- src/services/weatherApi.js
+++ src/services/weatherApi.js
@@ -30,5 +30,4 @@ class weatherApi {
     }
   }
 }
-
 export default new weatherApi();
